c5306fec6b30332a65cd58a8841bc1d31cb381de
José Antonio Zumaquero Torres <jazumaquero@gmail.com>
2017-01-27 14:00:42 +0100

    Updated Client and Domain from MailchimpDomain

--- campaingservice/src/main/scala/com/zcia/microservice/example/campaing/layer/MailchimpClient.scala
+++ campaingservice/src/main/scala/com/zcia/microservice/example/campaing/layer/MailchimpClient.scala
@@ -2,4 +2,13 @@ package com.zcia.microservice.example.campaing.layer
 
 trait MailchimpClient  extends BaseService with Protocol{
 
+  def createList(list: MailchimpDomain.List) : String
+
+  def createCampaign(campaign: MailchimpDomain.Campaign)
+  def createCampaignContent(content: MailchimpDomain.Content)
+
+  def addMembersToList(members: Seq[MailchimpDomain.Member])
+  def addMemberToCampaing(member: MailchimpDomain.Member)
+
+  def sendCampaing(campaign: MailchimpDomain.Campaign)
 }
--- campaingservice/src/main/scala/com/zcia/microservice/example/campaing/layer/MailchimpDomain.scala
+++ campaingservice/src/main/scala/com/zcia/microservice/example/campaing/layer/MailchimpDomain.scala
@@ -3,14 +3,15 @@ package com.zcia.microservice.example.campaing.layer
 /**
   * Created by ejoszum on 24/01/2017.
   */
-case class ListContact(company: String, address1: String, address2: String, city: String, state: String, zip: String, country: String, phone: String)
 
-case class ListCampaingDefaults(from_name: String, from_email: String, subject: String, language: String)
 
-case class List(name: String, contact: ListContact, permission_reminder: String, use_archive_bar: String, campaign_defaults: ListCampaingDefaults, notify_on_subscribe: String, notify_on_unsubscribe: String, email_type_option: Boolean, visibility: String)
-
-case class Location(latitude: Number, longitud: Number)
-
-case class Member(email_address: String, email_type: String, status: String, merge_fields: Object, interests: Object, language: String, vip: Boolean, location: Location, ip_signup: String, timestamp_signup: String, ip_opt: String, timestamp_opt: String)
-
-case class ListMembers(members: Seq[Member], update_existing: Boolean)
+object MailchimpDomain {
+  case class Member(email_address: String, status: String)
+  case class Contact(company: String, address1: String, city: String, state: String, zip: String, country: String)
+  case class CampaignDefaults(from_name: String, from_email: String, subject: String = "", language: String = "en")
+  case class List(id: Option[String], name: String, contact: Contact, permission_reminder: String, campaign_defaults: CampaignDefaults, email_type_option: Boolean = true)
+  case class Recipient(list_id: String)
+  case class Settings(subject_line: String, reply_to: String, from_name: String)
+  case class Campaign(id: String, recipients: Recipient, typE: String, settings: Settings)
+  case class Content(html: String)
+}
