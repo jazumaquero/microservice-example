1f1f67dd468612acc8d02b785239da4ce3373ca5
José Antonio Zumaquero Torres <jazumaquero@gmail.com>
2017-02-08 16:29:58 +0100

    Fix datalayer-jpa version, due to bad POJO was returned.

--- datalayer-jpa/src/main/java/com/zcia/microservice/example/repositories/SubscriberEventsRepository.java
+++ datalayer-jpa/src/main/java/com/zcia/microservice/example/repositories/SubscriberEventsRepository.java
@@ -6,18 +6,17 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.zcia.microservice.example.domain.ProductBuyEventEntity;
 import com.zcia.microservice.example.domain.SubscriberBuyEventEntity;
 
 @RepositoryRestResource(path = "subscribers_agg", collectionResourceRel = "subscribers_agg")
 public interface SubscriberEventsRepository extends PagingAndSortingRepository<SubscriberBuyEventEntity, Long>
 {
-    Page<ProductBuyEventEntity> findByCategoryId(@Param("id") Long id, Pageable page);
+    Page<SubscriberBuyEventEntity> findByCategoryId(@Param("id") Long id, Pageable page);
 
-    Page<ProductBuyEventEntity> findByCategoryName(@Param("name") String name, Pageable page);
+    Page<SubscriberBuyEventEntity> findByCategoryName(@Param("name") String name, Pageable page);
 
-    Page<ProductBuyEventEntity> findBySubscriberId(@Param("id") Long id, Pageable page);
+    Page<SubscriberBuyEventEntity> findBySubscriberId(@Param("id") Long id, Pageable page);
 
-    Page<ProductBuyEventEntity> findBySubscriberName(@Param("name") String name, Pageable page);
+    Page<SubscriberBuyEventEntity> findBySubscriberName(@Param("name") String name, Pageable page);
 
 }
